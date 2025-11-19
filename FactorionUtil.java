3ce675ca217104db71e8041b257e2a5285a4ff55
alifit24015-collab <banglaforce1971@gmail.com>
2025-11-19 21:40:35 +0600

    Add files via upload

---
+++ FactorionUtil.java
@@ -0,0 +1,25 @@
+public class FactorionUtil {
+
+    // Method to calculate factorial of a digit
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = 2; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    // Method to check if a number is a factorion
+    public static boolean isFactorion(int number) {
+        int originalNumber = number;
+        int sum = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            sum += factorial(digit);
+            number /= 10;
+        }
+
+        return sum == originalNumber;
+    }
+}
